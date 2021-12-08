15882a60272e246cc39e6122c5787763bbaf4428
Sameony <iam2001sam@gmail.com>
2021-12-09 00:06:04 +0530

    Updated symptoms

--- app.py
+++ app.py
@@ -21,15 +21,25 @@ model = predict_model()
 def main():
   st.title("Disease Prediction System Using Support Vector Machine")
   answer = []
-  features = ['skin_rash', 'continuous_sneezing', 'shivering', 'fatigue',
-       'irregular_sugar_level', 'high_fever', 'sweating', 'yellowish_skin',
-       'dark_urine', 'nausea', 'pain_behind_the_eyes', 'back_pain',
-       'constipation', 'diarrhoea', 'mild_fever', 'malaise',
-       'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose',
-       'congestion', 'obesity', 'loss_of_smell', 'toxic_look_(typhos)',
-       'muscle_pain', 'red_spots_over_body', 'belly_pain',
-       'watering_from_eyes', 'increased_appetite', 'polyuria',
-       'family_history', 'coma', 'stomach_bleeding', 'blood_in_sputum']
+  features = ['itching', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering',
+       'chills', 'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue',
+       'vomiting', 'burning_micturition', 'fatigue', 'weight_loss', 'lethargy',
+       'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating',
+       'dehydration', 'indigestion', 'headache', 'yellowish_skin',
+       'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes',
+       'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea',
+       'mild_fever', 'yellow_urine', 'yellowing_of_eyes',
+       'swelled_lymph_nodes', 'malaise', 'phlegm', 'throat_irritation',
+       'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion',
+       'chest_pain', 'fast_heart_rate', 'dizziness', 'muscle_weakness',
+       'swelling_joints', 'movement_stiffness', 'loss_of_balance',
+       'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine',
+       'continuous_feel_of_urine', 'depression', 'irritability', 'muscle_pain',
+       'red_spots_over_body', 'belly_pain', 'dischromic _patches',
+       'watering_from_eyes', 'increased_appetite', 'rusty_sputum',
+       'lack_of_concentration', 'visual_disturbances',
+       'receiving_blood_transfusion', 'blood_in_sputum', 'painful_walking',
+       'pus_filled_pimples', 'blackheads', 'scurring']
 
   for i in features:
     x = st.radio("Do you suffer from "+i+" ?",('Yes','No'))
